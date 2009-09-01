17dc4e07d69f778887a5e513f3c829bcd54fd7be
msassak <msassak@gmail.com>
2009-09-01 17:03:35 -0400

    Reorg specs to use listener

--- spec/gherkin/table_spec.rb
+++ spec/gherkin/table_spec.rb
@@ -2,6 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 module Gherkin
   describe Table do
+    def scan(text, expected)
+      listener = mock('listener')
+      listener.should_receive(:table).with(expected)
+      Table.new.scan(text, listener)
+    end
+    
     tables = {
       "|a|b|\n"        => [%w{a b}],
       "|a|b|c|\n"      => [%w{a b c}],
@@ -10,50 +16,45 @@ module Gherkin
     
     tables.each do |text, expected|
       it "should parse #{text}" do
-        listener = mock('listener')
-        listener.should_receive(:table).with(expected)
-        Table.new.scan(text, listener)
+        scan(text, expected)
       end
     end
     
-    def parse(text)
-      Table.new.parse(text)
-    end
-
     it "should parse a multicharacter cell content" do
-      parse("|foo|bar|\n").should == [%w{foo bar}]
+      # parse("|foo|bar|\n").should == [%w{foo bar}]
+      scan("|foo|bar|\n", [%w{foo bar}])
     end
     
     it "should parse a 1x2 table with newline" do
-      parse(" | 1 | 2 | \n").should == [%w{1 2}]
-    end
-
-    it "should parse a 1x2 table without newline" do
-      pending
-      parse("| 1 | 2 |").should == [%w{1 2}]
-    end
-
-    it "should parse a 1x2 table without spaces" do
-      pending
-      parse("|1|2|").should == [%w{1 2}]
+      scan(" | 1 | 2 | \n", [%w{1 2}])
     end
 
     it "should parse a 2x2 table" do
-      parse("| 1 | 2 |\n| 3 | 4 |\n").should == [%w{1 2}, %w{3 4}]
+      scan("| 1 | 2 |\n| 3 | 4 |\n", [%w{1 2}, %w{3 4}])
     end
 
     it "should parse a 2x2 table with several newlines" do
-      parse("| 1 | 2 |\n| 3 | 4 |\n\n\n").should == [%w{1 2}, %w{3 4}]
+      scan("| 1 | 2 |\n| 3 | 4 |\n\n\n", [%w{1 2}, %w{3 4}])
     end
 
     it "should parse a 2x2 table with empty cells" do
       pending
-      parse("| 1 |  |\n|| 4 |\n").should == [['1', nil], [nil, '4']]
+      scan("| 1 |  |\n|| 4 |\n", [['1', nil], [nil, '4']])
     end
 
     it "should not parse a 2x2 table that isn't closed" do
       pending
-      parse("| 1 |  |\n|| 4 ").should_not == [['1', nil], [nil, '4']]
+      scan("| 1 |  |\n|| 4 ", [['1', nil], [nil, '4']])
+    end
+    
+    it "should parse a 1x2 table without newline" do
+      pending
+      scan("| 1 | 2 |", [%w{1 2}])
+    end
+
+    it "should parse a 1x2 table without spaces" do
+      pending
+      scan("|1|2|", [%w{1 2}])
     end
   end
 end
