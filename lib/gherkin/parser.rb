e2c6174a39cd6ef24caf6f832ff75998f3a89d71
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-09-08 19:02:53 +0200

    Remove dead code (thanks RCov)

--- lib/gherkin/parser.rb
+++ lib/gherkin/parser.rb
@@ -1,15 +1,3 @@
 require 'gherkin/parser/feature'
 require 'gherkin/parser/table'
 require 'gherkin/parser/misc'
-
-module Gherkin
-  module Parser
-    extend self
-    
-    def parse_table(text, builder)
-      parser = Table.new(builder)
-      parser.scan(text)
-      builder
-    end    
-  end
-end
