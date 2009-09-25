4f897a6161b7f4004df964efef9b7cad54c9c7de
ghnatiuk <ghnatiuk@gmail.com>
2009-09-25 12:56:37 -0400

    Rename Feature to Parser

--- lib/gherkin/parser.rb
+++ lib/gherkin/parser.rb
@@ -15,12 +15,12 @@ module Gherkin
     
     def self.[](i18n_language)
       begin
-        require "gherkin/parser/feature_#{i18n_language}"
+        require "gherkin/parser/parser_#{i18n_language}"
       rescue LoadError
         raise I18nParserNotFound, "No parser was found for #{i18n_language}. Supported languages are listed in gherkin/i18n.yml."
       end
 
-      i18n_parser_class_name = i18n_language.gsub(/[\s-]/, '').capitalize + "Feature"
+      i18n_parser_class_name = i18n_language.gsub(/[\s-]/, '').capitalize + "Parser"
       parser_class = const_get(i18n_parser_class_name)
       parser_class
     end
