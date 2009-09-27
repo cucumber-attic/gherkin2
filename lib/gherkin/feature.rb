1c7aa422465f49c8506318997da74d1bde3b4ea0
msassak <msassak@gmail.com>
2009-09-27 13:59:15 -0400

    Rudimentary RDoc for Gherkin::Feature

--- lib/gherkin/feature.rb
+++ lib/gherkin/feature.rb
@@ -2,6 +2,13 @@ require 'gherkin/syntax_policy/feature_policy'
 require 'forwardable'
 
 module Gherkin
+  # Parses the Cucumber Feature format
+  # Sends events to the listener as they are received
+  #   <insert event list here>
+  # ParsingError will be raised if the parser itself cannot continue
+  # Syntax error if the text to scan is well formed, but syntactically incorrect
+  #   Gherkin::Feature.new('it', Listener.new) raises FeatureSyntaxError on error
+  #   Gherkin::Feature.new('en', AstBuilder.new, :raise_on_error => false) sends #syntax_error message to listener
   class Feature
     extend Forwardable
 
