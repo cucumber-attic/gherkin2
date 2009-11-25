4b62cf94c3ef8ef0fe6bad293815d530564bdc24
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-11-25 01:01:26 +0100

    Tweaked pretty printin feature so it gives better output. It's failing now on both Java and Ruby, slightly different. Common for both is extra content at the end.

--- features/step_definitions/pretty_printer_steps.rb
+++ features/step_definitions/pretty_printer_steps.rb
@@ -1,6 +1,20 @@
 require 'stringio'
 require 'gherkin/tools/pretty_printer'
 
+module PrettyPlease
+  def pretty(source)
+    io       = StringIO.new
+    listener = Gherkin::Tools::PrettyPrinter.new(io)
+    parser   = Gherkin::Parser.new(listener, true)
+    lexer    = Gherkin::I18nLexer.new(parser)
+    lexer.scan(source)
+    io.rewind
+    io.read
+  end
+end
+
+World(PrettyPlease  )
+
 Given /^I have Cucumber's home dir defined in CUCUMBER_HOME$/ do
   @cucumber_home = ENV['CUCUMBER_HOME']
   raise "No CUCUMBER_HOME" if @cucumber_home.nil?
@@ -13,18 +27,21 @@ end
 When /^I parse the prettified representation of each of them$/ do
   @errors = [['Path', 'Error']]
   @features.each do |feature|
-    feature_name = feature[@cucumber_home.length+1..-1]
-    io       = StringIO.new
-    listener = Gherkin::Tools::PrettyPrinter.new(io)
-    parser   = Gherkin::Parser.new(listener, true)
-    lexer    = Gherkin::I18nLexer.new(parser)
-    
     begin
-      lexer.scan(IO.read(feature))
-      io.rewind
-      pretty = io.read
+      pretty1 = pretty(IO.read(feature))
+      pretty2 = pretty(pretty1)
+
+      pretty2.should == pretty1
+    rescue Spec::Expectations::ExpectationNotMetError => e
+      announce "========== #{feature}:"
+      if(e.message =~ /(@@.*)/m)
+        announce $1
+      else
+        announce "??? NO DIFF ???"
+      end
+      @errors << [feature, "See announced diff"]
     rescue => e
-      @errors << [feature_name, e.message]
+      @errors << [feature, e.message]
     end
   end
 end
