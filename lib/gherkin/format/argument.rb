3b5983ba1d69c803d2dfb39b0c771e7b7fa95447
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2010-02-20 21:09:43 +0100

    Added a recent bugfix from Cucumber

--- lib/gherkin/format/argument.rb
+++ lib/gherkin/format/argument.rb
@@ -9,12 +9,13 @@ module Gherkin
       
       def self.format(string, args, &format)
         s = string.dup
-        offset = 0
+        offset = past_offset = 0
         args.each do |arg|
-          next if arg.byte_offset.nil?
+          next if arg.byte_offset.nil? || arg.byte_offset < past_offset
           replacement = format.call(arg.val)
           s[arg.byte_offset + offset, arg.val.length] = replacement
           offset += replacement.unpack("U*").length - arg.val.unpack("U*").length
+          past_offset = arg.byte_offset + arg.val.length
         end
         s
       end
--- spec/gherkin/format/argument_spec.rb
+++ spec/gherkin/format/argument_spec.rb
@@ -12,6 +12,8 @@ module Gherkin
       it "should replace one arg" do
         Argument.format("I have 10 cukes", [Argument.new(7, '10')], &method(:bracket).to_proc).should == "I have [10] cukes"
       end
+      
+      # TODO: Add this spec: http://github.com/alg/cucumber/commit/33188e9db51f59ced74c4861524d7b2e69454630
     end
   end
 end
