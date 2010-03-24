ab6ca51dbe6ef726abba552fcf01fe3802497b26
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-03-24 20:37:57 +0100

    Added convenience method for 3rd party syntax highlighters and editor plugins

--- lib/gherkin/i18n.rb
+++ lib/gherkin/i18n.rb
@@ -18,6 +18,23 @@ module Gherkin
       def languages
         @languages ||= {}
       end
+
+      # Returns all keyword translations and aliases of +keywords+, escaped and joined with <tt>|</tt>.
+      # This method is convenient for editor support and syntax highlighting engines for Gherkin, where
+      # there is typically a code generation tool to generate regular expressions for recognising the
+      # various I18n translations of Gherkin's keywords.
+      #
+      # The +keywords+ arguments can be one of <tt>:feature</tt>, <tt>:background</tt>, <tt>:scenario</tt>, 
+      # <tt>:scenario_outline</tt>, <tt>:examples</tt>, <tt>:step</tt>.
+      def keyword_regexp(*keywords)
+        unique_sorted_keywords = all.map do |lang|
+          keywords.map do |keyword|
+            lang.__send__("#{keyword}_keywords".to_sym)
+          end
+        end
+        
+        unique_sorted_keywords.flatten.compact.sort.reverse.uniq.join('|').gsub(/'/, "\\\\'").gsub(/\*/, "\\\\*")
+      end
     end
 
     attr_reader :key
@@ -79,6 +96,7 @@ module Gherkin
     end
 
     def adverbs
+      # TODO: looks very similar to #step_keywords. Lose this? Where is it used from?
       %w{given when then and but}.map{|keyword| @keywords[keyword].split('|').map{|w| w.gsub(/[\s<']/, '')}}.flatten
     end
 
