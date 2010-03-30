a1926bf1d8db8321e6968af7a7e60f08ef355473
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-03-30 16:55:58 +0200

    Trying again, with just a line filter

---
+++ lib/gherkin/tools/line_filter_listener.rb
@@ -0,0 +1,87 @@
+module Gherkin
+  module Tools
+    class LineFilterListener
+      def initialize(listener, lines)
+        @listener, @lines = listener, lines
+        @sexps = []
+        @filtered_sexps = []
+
+        @next_uncollected_scenario_index = 0
+        @current_index = -1
+      end
+
+      def method_missing(event, *args)
+        @current_index += 1
+        args[0] = args[0].to_a if event == :row # Special JRuby handling
+        sexp = [event] + args
+        @sexps << sexp
+        line = sexp[-1]
+        line_match = @lines.index(line)
+
+        case(event)
+        when :tag
+        when :comment
+        when :feature
+        when :scenario, :scenario_outline
+          @first_scenario ||= @current_index
+          @next_uncollected_scenario_index = @current_index
+          @scenario_ok = line_match
+        when :examples
+          @table_state = :examples
+          @examples_ok = line_match
+          @included_rows = {}
+        when :step
+          @table_state = :step
+          @scenario_ok ||= line_match
+          @included_rows = nil
+        when :row
+          case(@table_state)
+          when :examples
+            row_ok = line_match
+            if @included_rows.empty?
+              # The header row is always ok
+              @included_rows[@current_index] = true
+            else
+              @included_rows[@current_index] = @scenario_ok || @examples_ok || line_match
+            end
+          when :step
+            @scenario_ok ||= line_match
+          else
+            raise "BAD STATE"
+          end
+        when :eof
+        else
+          super
+        end
+
+        if @lines.empty?
+          @filtered_sexps << sexp
+        elsif @scenario_ok || @examples_ok || row_ok
+          collect_filtered_sexps
+        end
+      end
+
+      def lines
+        @filtered_sexps.map{|sexp| sexp[-1]}
+      end
+
+      def collect_filtered_sexps
+        # Collect Feature
+        if !@feature_added && @first_scenario
+          @filtered_sexps += @sexps[0...@first_scenario]
+          @feature_added = true
+        end
+
+        # Collect Scenario
+        (@next_uncollected_scenario_index..@current_index).each do |sexp_index|
+          sexp = @sexps[sexp_index]
+          event = sexp[0]
+          included_row = (event != :row) || @included_rows.nil? || @included_rows[sexp_index]
+          @filtered_sexps << sexp if included_row
+        end
+        
+        @next_uncollected_scenario_index = @current_index + 1
+      end
+    end
+  end
+end
---
+++ spec/gherkin/tools/line_filter_listener_spec.rb
@@ -0,0 +1,91 @@
+# encoding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require 'gherkin/tools/line_filter_listener'
+require 'stringio'
+
+module Gherkin
+  module Tools
+    describe LineFilterListener do
+      def verify_lines(expected_lines, lines)
+        fl = LineFilterListener.new(nil, lines)
+        parser = Gherkin::Parser.new(fl, true, "root")
+        lexer  = Gherkin::I18nLexer.new(parser, true)
+        lexer.scan(@input)
+        fl.lines.should == expected_lines
+      end
+
+      context "Scenario" do
+        before do
+          @input = %{Feature: 1
+
+  Scenario: 3
+    Given 4
+    When 5
+
+  Scenario: 7
+    Given 8
+    When 9
+     |10|10|
+     |11|11|
+}
+        end
+
+        it "should replay identically when there is no filter" do
+          verify_lines([1,3,4,5,7,8,9,10,11,:eof], [])
+        end
+
+        it "should filter on step line of first scenario" do
+          verify_lines([1,3,4,5], [5])
+        end
+
+        it "should filter on scenario line of second scenario" do
+          verify_lines([1,7,8,9,10,11,:eof], [7])
+        end
+      end
+
+      context "Scenario Outline" do
+        before do
+          @input = %{Feature: 1
+
+  Scenario Outline: 3
+    Given <foo> 4
+    When <bar> 5
+
+    Examples: 7
+      |foo|bar|
+      |  9|  9|
+      | 10| 10|
+      | 11| 11|
+      | 12| 12|
+      | 13| 13|
+
+    Examples: 15
+      |snip|snap|
+      |  17|  17|
+      |  18|  18|
+
+  Scenario: 17
+    Given 18
+    When 19
+}
+        end
+
+        it "should filter on step line of first scenario outline" do
+          verify_lines([1,3,4,5,7,8,9,10,11,12,13,15,16,17,18], [5])
+        end
+
+        it "should filter on row line of first scenario outline" do
+          verify_lines([1,3,4,5,7,8,11], [11])
+        end
+
+        it "should filter on examples line of second scenario outline" do
+          verify_lines([1,3,4,5,15,16,17,18], [15])
+        end
+
+        it "should filter on header row line of second scenario outline" do
+          verify_lines([1,3,4,5,15,17,18], [16])
+        end
+      end
+    end
+  end
+end
