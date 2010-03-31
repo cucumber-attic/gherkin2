7003b9a1ce8e0b41f38d2d81649af6e687397048
ghnatiuk <ghnatiuk@gmail.com>
2010-03-31 16:48:04 -0400

    start handling tags and backgrounds

--- lib/gherkin/tools/line_filter_listener.rb
+++ lib/gherkin/tools/line_filter_listener.rb
@@ -5,7 +5,7 @@ module Gherkin
     class LineFilterListener
       def initialize(listener, lines)
         @listener, @lines = listener, lines
-        @comment_buffer = []
+        @meta_buffer = []
         @feature_buffer = []
         @scenario_buffer = []
         @examples_buffer = []
@@ -19,32 +19,43 @@ module Gherkin
 
         case(sexp.event)
         when :tag
+          @meta_buffer << sexp
         when :comment
-          @comment_buffer << sexp
+          @meta_buffer << sexp
         when :feature
-          @feature_buffer = @comment_buffer + @feature_buffer
-          @comment_buffer = []
+          @feature_buffer = @meta_buffer + @feature_buffer
+          @meta_buffer = []
           @feature_buffer << sexp
         when :background
+          @feature_buffer += @meta_buffer + [sexp]
+          @meta_buffer = []
+          @table_state = :background
         when :scenario, :scenario_outline
           replay_examples_rows_buffer
-          @scenario_buffer = @comment_buffer
-          @comment_buffer = []
+          @scenario_buffer = @meta_buffer
+          @meta_buffer = []
           @scenario_buffer << sexp
           @scenario_ok = line_match?(sexp)
           @examples_ok = false
+          @table_state = :step
         when :examples
           replay_examples_rows_buffer
           
-          @examples_buffer = @comment_buffer + [sexp]
-          @comment_buffer = []
+          @examples_buffer = @meta_buffer + [sexp]
+          @meta_buffer = []
           @examples_rows_buffer = []
           @examples_ok = line_match?(sexp)
           @table_state = :examples
         when :step
-          @scenario_ok ||= line_match?(sexp)
-          @scenario_buffer << sexp
-          @table_state = :step
+          case(@table_state)
+          when :background
+            @feature_buffer += @meta_buffer + [sexp]
+            @meta_buffer = []
+          else
+            @scenario_ok ||= line_match?(sexp)
+            @scenario_buffer << sexp
+            @table_state = :step
+          end
         when :row
           case(@table_state)
           when :examples
@@ -57,6 +68,9 @@ module Gherkin
           when :step
             @scenario_buffer << sexp
             @scenario_ok ||= line_match?(sexp)
+          when :background
+            @feature_buffer += @meta_buffer + [sexp]
+            @meta_buffer = []
           else
             raise "BAD STATE"
           end
--- spec/gherkin/tools/line_filter_listener_spec.rb
+++ spec/gherkin/tools/line_filter_listener_spec.rb
@@ -69,7 +69,7 @@ module Gherkin
         end
       end
       
-      context "Scenario with Table and Comment" do
+      context "Scenario with Table and Comment and Tag" do
         before do
           @input = %{#language:en
 Feature: 2
@@ -78,16 +78,17 @@ Feature: 2
     Given 5
     When 6
 
-  Scenario: 8
-    Given 9
-    When 10
-     | 11 | 11 |
+  @tag8
+  Scenario: 9
+    Given 10
+    When 11
      | 12 | 12 |
+     | 13 | 13 |
 }
         end
 
         it "should replay identically when there is no filter" do
-          verify_lines([1,2,3,4,5,6,8,9,10,11,12,:eof], [])
+          verify_lines([1,2,3,4,5,6,8,9,10,11,12,13,:eof], [])
         end
 
         it "should filter on step line of first scenario" do
@@ -95,11 +96,41 @@ Feature: 2
         end
 
         it "should filter on scenario line of second scenario" do
-          verify_lines([1,2,8,9,10,11,12,:eof], [8])
+          verify_lines([1,2,8,9,10,11,12,13,:eof], [9])
         end
         
         it "should return everything when a line is given in each scenario" do
-          verify_lines([1,2,3,4,5,6,8,9,10,11,12,:eof], [6,8])
+          verify_lines([1,2,3,4,5,6,8,9,10,11,12,13,:eof], [6,9])
+        end
+      end
+      
+      context "Scenario with Background and Comment" do
+        before do
+          @input = %{#language:en
+Feature: 2
+  # 3
+  Background: 4
+    Given 5
+
+  # 7
+  Scenario: 8
+    Given 9
+    When 10
+
+  Scenario: 12
+    Given 13
+    When 14
+     | 15 | 16 |
+     | 15 | 16 |
+}
+        end
+
+        it "should replay identically when there is no filter" do
+          verify_lines([1,2,3,4,5,7,8,9,10,12,13,14,15,16,:eof], [])
+        end
+
+        it "should replay the background on step line of first scenario" do
+          verify_lines([1,2,3,4,5,7,8,9,10,:eof],[9])
         end
       end
 
@@ -107,67 +138,72 @@ Feature: 2
         before do
           @input = %{Feature: 1
 
-  Scenario Outline: 3
-    Given <foo> 4
-    When <bar> 5
+  @tag3
+  Scenario Outline: 4
+    Given <foo> 5
+    When <bar> 6
 
-    Examples: 7
+    @tag8
+    Examples: 9
       | foo | bar |
-      | 9   | 9   |
-      | 10  | 10  |
       | 11  | 11  |
       | 12  | 12  |
       | 13  | 13  |
+      | 14  | 14  |
+      | 15  | 15  |
 
-    Examples: 15
+    @tag17
+    Examples: 18
       | snip | snap |
-      | 17   | 17   |
-      | 18   | 18   |
+      | 20   | 20   |
+      | 21   | 21   |
 
-  Scenario: 20
-    Given 21
-    When 22
+  Scenario: 23
+    Given 24
+    When 25
 }
         end
 
         it "should filter on step line of first scenario outline" do
-          verify_lines([1,3,4,5,7,8,9,10,11,12,13,15,16,17,18,:eof], [5])
+          verify_lines([1,3,4,5,6,8,9,10,11,12,13,14,15,17,18,19,20,21,:eof], [6])
         end
 
         it "should filter on examples line of second scenario outline" do
-          verify_lines([1,3,4,5,15,16,17,18,:eof], [15])
+          verify_lines([1,3,4,5,6,17,18,19,20,21,:eof], [18])
         end
 
         it "should filter on header row line of second scenario outline" do
-          verify_lines([1,3,4,5,15,16,17,18,:eof], [16])
+          verify_lines([1,3,4,5,6,17,18,19,20,21,:eof], [19])
         end
 
         it "should filter on an example row of first scenario outline" do
-          verify_lines([1,3,4,5,7,8,11,:eof], [11])
+          verify_lines([1,3,4,5,6,8,9,10,13,:eof], [13])
         end
 
         it "should filter on an example row of second scenario outline" do
-          verify_lines([1,3,4,5,15,16,18,:eof], [18])
+          verify_lines([1,3,4,5,6,17,18,19,20,:eof], [20])
         end
 
         it "should filter on 2 example rows of first scenario outline" do
-          verify_lines([1,3,4,5,7,8,11,13,:eof], [11,13])
+          verify_lines([1,3,4,5,6,8,9,10,12,14,:eof], [12,14])
         end
 
         it "should replay itself properly" do
           filtered = %{Feature: 1
 
-  Scenario Outline: 3
-    Given <foo> 4
-    When <bar> 5
+  @tag3
+  Scenario Outline: 4
+    Given <foo> 5
+    When <bar> 6
 
-    Examples: 7
+    @tag8
+    Examples: 9
       | foo | bar |
-      | 11  | 11  |
-      | 13  | 13  |
+      | 12  | 12  |
+      | 14  | 14  |
 }
 
-          verify_output(filtered, [11,13])
+          verify_output(filtered, [12,14])
         end
       end
     end
