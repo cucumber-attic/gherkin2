322cfd4c7d7e3a0541fc3ea12956fd2bf0515da6
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-04-14 23:58:10 +0200

    All but one spec passing, using ruby FilterListener and Java TagExpression. Not sure where the problem is.

--- lib/gherkin/java_impl.rb
+++ lib/gherkin/java_impl.rb
@@ -7,13 +7,21 @@ class Class
     if defined?(JRUBY_VERSION)
       require jar
       class << self
-        define_method(:new) do |*args|
+        def new(*args)
+          java_class.new(*args)
+        end
+
+        def ===(object)
+          super || object.java_kind_of?(java_class)
+        end
+
+        def java_class
           names = self.name.split('::')
           package = Java
           names[0..-2].each do |module_name|
             package = package.__send__(module_name.downcase)
           end
-          package.__send__(names[-1]).new(*args)
+          package.__send__(names[-1])
         end
       end
     end
