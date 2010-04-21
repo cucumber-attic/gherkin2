1d3ddb689470ad0762292a6521d48a479b557dd4
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-04-21 20:14:03 +0200

    Added another test - one that reads from string in source, and another from file.

--- java/src/test/java/gherkin/I18nLexerTest.java
+++ java/src/test/java/gherkin/I18nLexerTest.java
@@ -2,11 +2,13 @@ package gherkin;
 
 import org.junit.Test;
 
+import java.io.InputStreamReader;
+
 import static org.mockito.Mockito.*;
 
 public class I18nLexerTest {
     @Test
-    public void shouldScanUtf8FeaturesProperly() {
+    public void shouldScanUtf8FeatureInSourceProperly() {
         Listener listener = mock(Listener.class);
         Lexer lexer = new I18nLexer(listener);
 
@@ -16,6 +18,23 @@ public class I18nLexerTest {
                 "    Given I have an empty stack\n" +
                 "    When I pøsh <x> onto the stack";
 
+        System.out.println("SCANNING SOURCE STRING:");
+        System.out.println(feature);
+        lexer.scan(feature);
+
+        verify(listener).feature("Feature", "ÆØÅ", 1);
+        verify(listener).step("When ", "I pøsh <x> onto the stack", 5);
+    }
+
+    @Test
+    public void shouldScanUtf8FeatureInFileProperly() {
+        Listener listener = mock(Listener.class);
+        Lexer lexer = new I18nLexer(listener);
+
+        String feature = FixJava.readReader(new InputStreamReader(getClass().getResourceAsStream("/gherkin/utf8.feature")));
+
+        System.out.println("SCANNING FILE RESOURCE:");
+        System.out.println(feature);
         lexer.scan(feature);
 
         verify(listener).feature("Feature", "ÆØÅ", 1);
---
+++ java/src/test/resources/gherkin/utf8.feature
@@ -0,0 +1,5 @@
+Feature: ÆØÅ
+
+  Scenario Outline:
+    Given I have an empty stack
+    When I pøsh <x> onto the stack
