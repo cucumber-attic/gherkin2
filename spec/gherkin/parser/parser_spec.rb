1e88c684f745445bf53eb81689735a23b247ce1c
Matt Wynne <matt@mattwynne.net>
2010-06-10 22:58:27 +0100

    Fix some obvious jruby spec failures

--- spec/gherkin/parser/parser_spec.rb
+++ spec/gherkin/parser/parser_spec.rb
@@ -16,7 +16,7 @@ module Gherkin
 
       it "should raise helpful error messages by default" do
         lambda { 
-          @parser.scenario("Scenario", "My pet scenario", 12) 
+          @parser.scenario("Scenario", "My pet scenario", "", 12) 
         }.should raise_error(/Parse error on line 12\. Found scenario when expecting one of: comment, feature, tag\. \(Current state: root\)\.$/)
       end
 
@@ -28,7 +28,7 @@ module Gherkin
       it "should delegate an error message when raise on error is false" do
         @listener.should_receive(:syntax_error).with(sym(:root), sym(:background), a([:comment, :feature, :tag]), 1)
         @parser = Parser.new(@listener, false)
-        @parser.background("Background", "Content", 1)
+        @parser.background("Background", "Content", "", 1)
       end
 
       [true, false].each do |native|
