f6b3a5e5f949c5e8fd05f1f306d50d38cedc3ce2
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-06-16 04:29:04 +0200

    Javadoc

--- java/src/main/java/gherkin/Lexer.java
+++ java/src/main/java/gherkin/Lexer.java
@@ -1,5 +1,12 @@
 package gherkin;
 
 public interface Lexer {
-    public void scan(String gherkinSource, String uri, int location);
+    /**
+     * Lexes the source.
+     *
+     * @param source a string containing Gherkin source.
+     * @param uri the URI where the source originated from. Typically a file path.
+     * @param offset the line offset within the uri document the source was taken from. Typically 0.
+     */
+    public void scan(String source, String uri, int offset);
 }
