543a45d4d91c5dd82bdce603e9d9b083b424521b
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-06-28 23:55:01 +0200

    Added a TagCountFormatter

---
+++ lib/gherkin/formatter/tag_count_formatter.rb
@@ -0,0 +1,44 @@
+module Gherkin
+  module Formatter
+    class TagCountFormatter
+      attr_reader :tag_counts
+      
+      def initialize(formatter)
+        @formatter = formatter
+        @tag_counts = Hash.new{|hash, tag| hash[tag] = []}
+      end
+
+      def feature(comments, tags, keyword, name, description, uri)
+        @feature_tags = tags
+        @uri = uri
+      end
+
+      def scenario(comments, tags, keyword, name, description, line)
+        record_tags((@feature_tags.to_a + tags.to_a).uniq, line)
+        @formatter.__send__(:scenario, comments, tags, keyword, name, description, line)
+      end
+
+      def scenario_outline(comments, tags, keyword, name, description, line)
+        @scenario_outline_tags = tags
+        @formatter.__send__(:scenario_outline, comments, tags, keyword, name, description, line)
+      end
+
+      def examples(comments, tags, keyword, name, description, line, examples_table)
+        record_tags((@feature_tags.to_a + @scenario_outline_tags.to_a + tags.to_a).uniq, line)
+        @formatter.__send__(:examples, tags, keyword, name, description, line)
+      end
+
+    private
+
+      def record_tags(tags, line)
+        tags.each do |tag|
+          @tag_counts[tag] << "#{@uri}:#{line}"
+        end
+      end
+
+      def method_missing(*args)
+        @formatter.__send__(*args)
+      end
+    end
+  end
+end
---
+++ spec/gherkin/formatter/tag_count_formatter_spec.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+require 'spec_helper'
+require 'gherkin/i18n_lexer'
+require 'gherkin/listener/formatter_listener'
+require 'gherkin/formatter/tag_count_formatter'
+
+module Gherkin
+  module Formatter
+    describe TagCountFormatter do
+      it "should count tags" do
+        dummy = Gherkin::SexpRecorder.new
+        @formatter = Gherkin::Formatter::TagCountFormatter.new(dummy)
+        fl = Gherkin::Listener::FormatterListener.new(@formatter)
+        @lexer = Gherkin::I18nLexer.new(fl)
+
+        @lexer.scan(File.new(File.dirname(__FILE__) + "/../fixtures/complex_with_tags.feature").read, "complex.feature", 0)
+        
+        @formatter.tag_counts.should == {
+          "@hamster" => ["complex.feature:58"],
+          "@tag1"    => ["complex.feature:18","complex.feature:23","complex.feature:39","complex.feature:52","complex.feature:58"],
+          "@tag2"    => ["complex.feature:18","complex.feature:23","complex.feature:39","complex.feature:52","complex.feature:58"],
+          "@tag3"    => ["complex.feature:18", "complex.feature:23"],
+          "@tag4"    => ["complex.feature:18"],
+          "@neat"    => ["complex.feature:52"],
+          "@more"    => ["complex.feature:52", "complex.feature:58"]
+        }
+      end
+    end
+  end
+end
