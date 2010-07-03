47349f314b8b1f9649cd2e2a0b5e0085ae073c80
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-07-03 10:41:31 +0200

    Extract spec helper method

--- spec/gherkin/formatter/filter_formatter_spec.rb
+++ spec/gherkin/formatter/filter_formatter_spec.rb
@@ -9,19 +9,22 @@ require 'gherkin/formatter/pretty_formatter'
 module Gherkin
   module Formatter
     describe FilterFormatter do
-      it "should filter on tags" do
-        filters = ['@tag4']
-        
+      def verify_filter(filters, lines)
         io = StringIO.new
         pretty_formatter = Gherkin::Formatter::PrettyFormatter.new(io, true)
-        @formatter = Gherkin::Formatter::FilterFormatter.new(pretty_formatter, filters)
-        fl = Gherkin::Listener::FormatterListener.new(@formatter)
-        @lexer = Gherkin::I18nLexer.new(fl)
+        formatter = Gherkin::Formatter::FilterFormatter.new(pretty_formatter, filters)
+        fl = Gherkin::Listener::FormatterListener.new(formatter)
+        lexer = Gherkin::I18nLexer.new(fl)
 
         source = File.new(File.dirname(__FILE__) + "/../fixtures/complex_for_filtering.feature").read
-        @lexer.scan(source, "complex.feature", 0)
+        lexer.scan(source, "complex.feature", 0)
         
-        io.string.should == source.split("\n")[0..18].join("\n")
+        range = (lines.first-1..lines.last-1)
+        io.string.should == source.split("\n")[range].join("\n")
+      end
+
+      it "should filter on tags" do
+        verify_filter(['@tag4'], 1..19)
       end
     end
   end
