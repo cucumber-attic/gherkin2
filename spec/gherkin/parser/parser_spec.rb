d41f201eb93dbab0c09ddfda2ea4d51216496583
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-07-14 15:08:25 +0200

    Release 2.1.4

--- spec/gherkin/parser/parser_spec.rb
+++ spec/gherkin/parser/parser_spec.rb
@@ -9,13 +9,6 @@ module Gherkin
           p.parse("Feature: f\nFeature: f", __FILE__, __LINE__-1)
         end.should raise_error(/Parse error at/)
       end
-
-      it "should raise when steps don't parse" do
-        p = Parser.new(mock('formatter').as_null_object, true, 'steps', false)
-        lambda do
-          p.parse("", __FILE__, __LINE__-1)
-        end.should raise_error(/Parse error at/)
-      end
     end
   end
 end
