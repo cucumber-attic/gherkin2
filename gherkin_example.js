7893d2442634a4e71cb7dae6bbfd0840ad3ae515
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-14 23:25:44 +0000

    Nicer interface

--- gherkin_example.js
+++ gherkin_example.js
@@ -1,30 +1,34 @@
-var fs = require('fs')
-var Lexer = new require('./js/gherkin.en').Lexer;
-var lexer = new Lexer(null);
+var fs = require('fs');
+var Lexer = require('./js/gherkin.en').Lexer;
 
-lexer.on('feature', function(keyword, name, description, line) {
-  console.log(keyword + ': ' + name);
-});
-lexer.on('background', function(keyword, name, description, line) {
-  console.log('  ' + keyword + ': ' + name);
-});
-lexer.on('scenario', function(keyword, name, description, line) {
-  console.log('  ' + keyword + ': ' + name);
-});
-lexer.on('scenario_outline', function(keyword, name, description, line) {
-  console.log('  ' + keyword + ': ' + name);
-});
-lexer.on('examples', function(keyword, name, description, line) {
-  console.log('    ' + keyword + ': ' + name);
-});
-lexer.on('step', function(keyword, name, line) {
-  console.log('    ' + keyword + name);
-});
-lexer.on('py_string', function(string, line) {
-  console.log('      """\n' + string + '\n      """');
-});
-lexer.on('row', function(row, line) {
-  console.log('      | ' + row.join(' | ') + ' |');
+var lexer = new Lexer({
+  feature: function(keyword, name, description, line) {
+    console.log(keyword + ': ' + name);
+  },
+  background: function(keyword, name, description, line) {
+    console.log('  ' + keyword + ': ' + name);
+  },
+  scenario: function(keyword, name, description, line) {
+    console.log('  ' + keyword + ': ' + name);
+  },
+  scenario_outline: function(keyword, name, description, line) {
+    console.log('  ' + keyword + ': ' + name);
+  },
+  examples: function(keyword, name, description, line) {
+    console.log('  ' + keyword + ': ' + name);
+  },
+  step: function(keyword, name, line) {
+    console.log('    ' + keyword + name);
+  },
+  py_string: function(string, line) {
+    console.log('      """\n' + string + '\n      """');
+  },
+  row: function(row, line) {
+    console.log('      | ' + row.join(' | ') + ' |');
+  },
+  end: function() {
+    console.log('=====');
+  }
 });
 
 lexer.scan(fs.readFileSync(process.ARGV[2]));
