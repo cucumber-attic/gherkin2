f63e03cd4143199c46d906556af4b396f53bf5f9
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-19 00:18:21 +0000

    Wrap the js lexer in a ruby class. -But why is nothing lexing??

--- js/example/print.rb
+++ js/example/print.rb
@@ -1,8 +1,27 @@
 require 'v8'
 
-cxt = V8::Context.new
-cxt['exports'] = {}
-cxt.load(File.dirname(__FILE__) + '/../lib/gherkin/lexer/i18n/en.js')
+module Gherkin
+  class JSLexer
+    def initialize(listener, lang='en')
+      @cxt = V8::Context.new
+      @cxt['exports'] = {}
+
+      # Mimic Node.js / Firebug console.log
+      @cxt['console'] = STDOUT
+      def STDOUT.log(*a)
+        puts(*a)
+      end
+
+      @cxt.load(File.dirname(__FILE__) + "/../lib/gherkin/lexer/i18n/#{lang}.js")
+
+      @lexer = @cxt['exports']['Lexer'].new(listener)
+    end
+
+    def scan(gherkin)
+      @lexer['scan'].call(gherkin)
+    end
+  end
+end
 
 class Listener
   ['feature', 'background', 'scenario', 'scenario_outline', 'examples', 'step', 'py_string', 'row', 'end'].each do |m|
@@ -12,5 +31,4 @@ class Listener
   end
 end
 
-lexer = cxt['exports']['Lexer'].new(Listener.new)
-lexer['scan'].call("Feature:\n Hello")
+Gherkin::JSLexer.new(Listener.new).scan("Feature:\n Hello\nScenario: DUDE\n")
