8bb791e3916723d78fb1f4949ef5487a5f383006
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-19 23:58:28 +0000

    Code is working on therubyracer

--- README_JAVASCRIPT.md
+++ README_JAVASCRIPT.md
@@ -3,13 +3,10 @@ Building and Running the js lexer
 
 You need Node.js and http://github.com/dominicmarks/ragel-js installed. Then:
 
-  rake js/lib/gherkin/lexer/i18n/en.js && node js/example/print.js features/json_formatter.feature
+  rake js/lib/gherkin/lexer/i18n/en.js && node js/example/print.js spec/gherkin/fixtures/1.feature
 
 Or: (using therubyracer)
 
-  # TODO: Why doesn't this print anything?? Sprinkle some console.log statements in the scan functio
-  # to verify that we actually get into the scan function. However - from there nothing seems to happen.
-  #
-  rake js/lib/gherkin/lexer/i18n/en.js && ruby js/example/print.rb features/json_formatter.feature
+  rake js/lib/gherkin/lexer/i18n/en.js && ruby js/example/print.rb spec/gherkin/fixtures/1.feature 
 
-That prints gherkin to stdout - almost like the original.
+That prints gherkin to stdout - almost like the original. Well, not quite, but you get the idea. We're firing events.
--- js/example/print.rb
+++ js/example/print.rb
@@ -9,7 +9,7 @@ module Gherkin
       # Mimic Node.js / Firebug console.log
       @cxt['console'] = STDOUT
       def STDOUT.log(*a)
-        puts(*a)
+        p a
       end
 
       @cxt.load(File.dirname(__FILE__) + "/../lib/gherkin/lexer/i18n/#{lang}.js")
@@ -18,17 +18,38 @@ module Gherkin
     end
 
     def scan(gherkin)
-      @lexer['scan'].call(gherkin)
+      @lexer['scan'].methodcall(@lexer, gherkin)
     end
   end
 end
 
 class Listener
-  ['feature', 'background', 'scenario', 'scenario_outline', 'examples', 'step', 'py_string', 'row', 'end'].each do |m|
-    define_method(m) do |*a|
-      p [m] + a
-    end
+  def feature(keyword, name, description, line)
+    puts(keyword + ': ' + name)
+  end
+  def background(keyword, name, description, line)
+    puts('  ' + keyword + ': ' + name)
+  end
+  def scenario(keyword, name, description, line)
+    puts('  ' + keyword + ': ' + name)
+  end
+  def scenario_outline(keyword, name, description, line)
+    puts('  ' + keyword + ': ' + name)
+  end
+  def examples(keyword, name, description, line)
+    puts('  ' + keyword + ': ' + name)
+  end
+  def step(keyword, name, line)
+    puts('    ' + keyword + name)
+  end
+  def py_string(string, line)
+    puts('      """\n' + string + '\n      """')
+  end
+  def row(row, line)
+    puts('      | ' + row.join(' | ') + ' |')
+  end
+  def end
+    puts('=====')
   end
 end
-
-Gherkin::JSLexer.new(Listener.new).scan(IO.read(ARGV[0]))
+Gherkin::JSLexer.new(Listener.new).scan(IO.read(ARGV[0]).unpack("c*") )
