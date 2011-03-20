8784b25a9d26693320d24c07c92dda7dd39930da
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-03-20 01:01:21 +0000

    Gherkin is running in the browser. Piece of cake.

--- js/example/print.rb
+++ js/example/print.rb
@@ -2,20 +2,28 @@ $:.unshift(File.dirname(__FILE__) + '/../../spec')
 require 'gherkin/js_lexer'
 
 class SimplePrettyPrinterListener
+  def comment(value, line)
+    puts(value)
+  end
   def feature(keyword, name, description, line)
     puts(keyword + ': ' + name)
+    puts('  ' + description) if description.to_s != ""
   end
   def background(keyword, name, description, line)
     puts('  ' + keyword + ': ' + name)
+    puts('  ' + description) if description.to_s != ""
   end
   def scenario(keyword, name, description, line)
     puts('  ' + keyword + ': ' + name)
+    puts('  ' + description) if description.to_s != ""
   end
   def scenario_outline(keyword, name, description, line)
     puts('  ' + keyword + ': ' + name)
+    puts('  ' + description) if description.to_s != ""
   end
   def examples(keyword, name, description, line)
     puts('  ' + keyword + ': ' + name)
+    puts('    ' + description) if description.to_s != ""
   end
   def step(keyword, name, line)
     puts('    ' + keyword + name)
@@ -31,4 +39,4 @@ class SimplePrettyPrinterListener
   end
 end
 
-Gherkin::JsLexer.new(SimplePrettyPrinterListener.new).scan(IO.read(ARGV[0]).unpack("c*"))
+Gherkin::JsLexer.new(SimplePrettyPrinterListener.new).scan(IO.read(ARGV[0]))
