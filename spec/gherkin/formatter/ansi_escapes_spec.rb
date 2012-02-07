a44b813a28800f85251cb86dc3d0b66e4c5477bf
Ben Woosley <ben.woosley@gmail.com>
2012-02-07 11:05:42 -0800

    Expose AnsiEscapes methods on the class level as well, for easy calling without inclusion

--- spec/gherkin/formatter/ansi_escapes_spec.rb
+++ spec/gherkin/formatter/ansi_escapes_spec.rb
@@ -4,16 +4,29 @@ require 'gherkin/formatter/ansi_escapes'
 module Gherkin
   module Formatter
     describe AnsiEscapes do
-      include Gherkin::Formatter::AnsiEscapes
+      describe "instance methods" do
+        include AnsiEscapes
 
-      it "failed should be red" do
-        failed.should == "\e[31m"
+        it "failed should be red" do
+          failed.should == "\e[31m"
+        end
+
+        it "failed_arg should be red bold" do
+          failed_arg.should == "\e[31m\e[1m"
+        end
       end
 
-      it "failed arg should be red bold" do
-        failed_arg.should == "\e[31m\e[1m"
-      end
+      describe "class methods" do
+        subject { AnsiEscapes }
 
+        it "failed should be red" do
+          subject.failed.should == "\e[31m"
+        end
+
+        it "failed_arg should be red bold" do
+          subject.failed_arg.should == "\e[31m\e[1m"
+        end
+      end
     end
   end
 end
