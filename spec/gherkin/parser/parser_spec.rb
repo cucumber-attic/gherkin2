cfe0dcdbd9929f8eb78bfcaf30fb2e3a54a22df1
Lukas Oberhuber <lukaso@gmail.com>
2013-04-02 09:58:46 +0100

    Fix for issue #247, file not displayed on error.

--- spec/gherkin/parser/parser_spec.rb
+++ spec/gherkin/parser/parser_spec.rb
@@ -6,9 +6,10 @@ module Gherkin
       unless defined?(JRUBY_VERSION)
         it "should raise when feature doesn't parse" do
           p = Parser.new(mock('formatter').as_null_object)
+          err = Regexp.new("Parse error at #{__FILE__}:\\d+")
           lambda do
             p.parse("Feature: f\nFeature: f", __FILE__, __LINE__-1)
-          end.should raise_error(/Parse error at/)
+          end.should raise_error(err)
         end
       end
     end
