5850196fd7d3341e3b2398518412906798dd4932
Lukas Oberhuber <lukaso@gmail.com>
2013-04-02 11:13:19 +0100

    Tiny cleanup

--- spec/gherkin/parser/parser_spec.rb
+++ spec/gherkin/parser/parser_spec.rb
@@ -6,10 +6,9 @@ module Gherkin
       unless defined?(JRUBY_VERSION)
         it "should raise when feature doesn't parse" do
           p = Parser.new(mock('formatter').as_null_object)
-          err = Regexp.new("Parse error at #{__FILE__}:\\d+")
           lambda do
             p.parse("Feature: f\nFeature: f", __FILE__, __LINE__-1)
-          end.should raise_error(err)
+          end.should raise_error(Regexp.new("Parse error at #{__FILE__}:\\d+"))
         end
       end
     end
