0f1137624a5efd808c26c2571b3cf0d2f8479e18
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2013-04-09 13:06:01 +0100

    Fixed mingw install script

--- install_mingw_os_x.sh
+++ install_mingw_os_x.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-
 mkdir -p /usr/local/mingw
-cd /usr/local/mingw
-# Don't attempt any of the newer versions - they don't work (gcc 4.7.0)
-wget http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Automated%20Builds/mingw-w32-1.0-bin_i686-darwin_20110819.tar.bz2
-tar xvfj mingw-w32-1.0-bin_i686-darwin_20110819.tar.bz2
+
+# We need GCC 4.7 in order to build ruby 1.8.7. When we drop support for that we can use a more recent one
+cd /usr/local/mingw && curl --silent --location http://downloads.sourceforge.net/project/mingw-w64/Toolchains%20targetting%20Win32/Automated%20Builds/mingw-w32-1.0-bin_i686-darwin_20120227.tar.bz2 | tar xvj
