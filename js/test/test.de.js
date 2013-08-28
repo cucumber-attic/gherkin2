b53360ad0d0fba455cbe7bcc5a0129d4814cff36
Thorsten Glaser <t.glaser@tarent.de>
2013-08-28 17:14:59 +0200

    clean up for submission
    - use indentation style of surrounding code
    - avoid unnecessary whitespace changes
    - more comments
    - add names to LICENCE file

--- js/test/test.de.js
+++ js/test/test.de.js
@@ -56,20 +56,20 @@ describe("Lexer",function(){
       assert.equal(recorder.records[0].name,"Jede Menge €€€s!");
     });
   });
-  
+
   describe('#scan with Buffer',function(){
     it("should accept keywords that include non-ascii characters",function(){
       var recorder=Recorder();
       var lexer=new Lexer(recorder);
       assert.doesNotThrow(function(){
-        //same string as above, utf-8-encoded
+        // same string as above, UTF-8 encoded
 	var utf8_data = [
-          0x46, 0x75, 0x6e, 0x6b, 0x74, 0x69, 0x6f, 0x6e,  
+          0x46, 0x75, 0x6e, 0x6b, 0x74, 0x69, 0x6f, 0x6e,
           0x61, 0x6c, 0x69, 0x74, 0xc3, 0xa4, 0x74, 0x3a,
-          0x20, 0x4a, 0x65, 0x64, 0x65, 0x20, 0x4d, 0x65,  
+          0x20, 0x4a, 0x65, 0x64, 0x65, 0x20, 0x4d, 0x65,
           0x6e, 0x67, 0x65, 0x20, 0xe2, 0x82, 0xac, 0xe2,
           0x82, 0xac, 0xe2, 0x82, 0xac, 0x73, 0x21, 0x0a
-        ]; 
+        ];
 
         lexer.scan(new Buffer(utf8_data));
       });
@@ -79,6 +79,5 @@ describe("Lexer",function(){
       assert.equal(recorder.records[0].name,"Jede Menge €€€s!");
     });
   });
-
 
 });
