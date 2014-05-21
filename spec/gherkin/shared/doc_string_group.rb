e26ccc883e6b2298f3718e66cd5685009f631610
John Voloski <johnvoloski@cwi.com.br>
2014-05-21 12:49:32 -0300

    doing update from shared's rspec's syntax

--- spec/gherkin/shared/doc_string_group.rb
+++ spec/gherkin/shared/doc_string_group.rb
@@ -9,8 +9,8 @@ module Gherkin
       def ps(content)
         '"""%s"""' % ("\n" + content + "\n")
       end
-      
-      it "should provide the amount of indentation of the triple quotes to the listener" do
+
+      it "provides the amount of indentation of the triple quotes to the listener" do
 str = <<EOS
 Feature: some feature
   Scenario: some scenario
@@ -21,21 +21,21 @@ Feature: some feature
     """
     Then bar
 EOS
-        @listener.should_receive(:doc_string).with('', "  Hello\nGoodbye", 4)
+        expect(@listener).to receive(:doc_string).with('', "  Hello\nGoodbye", 4)
         scan(str)
       end
 
-      it "should parse a simple doc_string" do
-        @listener.should_receive(:doc_string).with('', "I am a doc_string", 1)
+      it "parses a simple doc_string" do
+        expect(@listener).to receive(:doc_string).with('', "I am a doc_string", 1)
         scan ps("I am a doc_string")
       end
 
-      it "should parse an empty doc_string" do
-        @listener.should_receive(:doc_string).with('', '', 4)
+      it "parses an empty doc_string" do
+        expect(@listener).to receive(:doc_string).with('', '', 4)
         scan("Feature: Hi\nScenario: Hi\nGiven a step\n\"\"\"\n\"\"\"")
       end
 
-      it "should treat a string containing only newlines as only newlines" do
+      it "treats a string containing only newlines as only newlines" do
 doc_string = <<EOS
 """
 
@@ -43,45 +43,45 @@ doc_string = <<EOS
 
 """
 EOS
-        @listener.should_receive(:doc_string).with('', "\n\n", 1)
+        expect(@listener).to receive(:doc_string).with('', "\n\n", 1)
         scan(doc_string)
       end
-      
-      it "should parse content separated by two newlines" do
+
+      it "parses content separated by two newlines" do
         scan ps("A\n\nB")
-        @listener.to_sexp.should == [
+        expect(@listener.to_sexp).to eq([
           [:doc_string, '', "A\n\nB", 1],
           [:eof]
-        ]
+        ])
       end
-      
-      it "should parse a multiline string" do
-        @listener.should_receive(:doc_string).with('', "A\nB\nC\nD", 1)
+
+      it "parses a multiline string" do
+        expect(@listener).to receive(:doc_string).with('', "A\nB\nC\nD", 1)
         scan ps("A\nB\nC\nD")
       end
-            
-      it "should ignore unescaped quotes inside the string delimeters" do
-        @listener.should_receive(:doc_string).with('', "What does \"this\" mean?", 1)
+
+      it "ignores unescaped quotes inside the string delimeters" do
+        expect(@listener).to receive(:doc_string).with('', "What does \"this\" mean?", 1)
         scan ps('What does "this" mean?')
       end
-      
-      it "should preserve whitespace within the triple quotes" do
+
+      it "preserves whitespace within the triple quotes" do
 str = <<EOS
     """
       Line one
  Line two
     """
 EOS
-        @listener.should_receive(:doc_string).with('', "  Line one\nLine two", 1)
+        expect(@listener).to receive(:doc_string).with('', "  Line one\nLine two", 1)
         scan(str)
       end
-            
-      it "should preserve tabs within the content" do
-        @listener.should_receive(:doc_string).with('', "I have\tsome tabs\nInside\t\tthe content", 1)
+
+      it "preserves tabs within the content" do
+        expect(@listener).to receive(:doc_string).with('', "I have\tsome tabs\nInside\t\tthe content", 1)
         scan ps("I have\tsome tabs\nInside\t\tthe content")
       end
-  
-      it "should handle complex doc_strings" do
+
+      it "handles complex doc_strings" do
 doc_string = <<EOS
 # Feature comment
 @one
@@ -95,22 +95,22 @@ Feature: Sample
 1 step (1 passed)
 
 EOS
-        
-        @listener.should_receive(:doc_string).with('', doc_string, 1)
+
+        expect(@listener).to receive(:doc_string).with('', doc_string, 1)
         scan ps(doc_string)
       end
- 
-      it "should allow whitespace after the closing doc_string delimiter" do
+
+      it "allows whitespace after the closing doc_string delimiter" do
 str = <<EOS
     """
       Line one
-    """           
+    """
 EOS
-        @listener.should_receive(:doc_string).with('', "  Line one", 1)
+        expect(@listener).to receive(:doc_string).with('', "  Line one", 1)
         scan(str)
       end
 
-      it "should preserve the last newline(s) at the end of a doc_string" do
+      it "preserves the last newline(s) at the end of a doc_string" do
 str = <<EOS
      """
      DocString text
@@ -118,42 +118,42 @@ str = <<EOS
 
      """
 EOS
-        @listener.should_receive(:doc_string).with('', "DocString text\n\n", 1)
+        expect(@listener).to receive(:doc_string).with('', "DocString text\n\n", 1)
         scan(str)
-      end        
+      end
 
-      it "should preserve CRLFs within doc_strings" do
-        @listener.should_receive(:doc_string).with('', "Line one\r\nLine two\r\n", 1)
+      it "preserves CRLFs within doc_strings" do
+        expect(@listener).to receive(:doc_string).with('', "Line one\r\nLine two\r\n", 1)
         scan("\"\"\"\r\nLine one\r\nLine two\r\n\r\n\"\"\"")
       end
 
-      it "should unescape escaped triple quotes" do
+      it "unescapes escaped triple quotes" do
 str = <<EOS
     """
     \\"\\"\\"
     """
 EOS
-        @listener.should_receive(:doc_string).with('', '"""', 1)
+        expect(@listener).to receive(:doc_string).with('', '"""', 1)
         scan(str)
       end
 
-      it "should not unescape escaped single quotes" do
+      it "does not unescape escaped single quotes" do
 str = <<EOS
     """
     \\" \\"\\"
     """
 EOS
-        @listener.should_receive(:doc_string).with('', '\" \"\"', 1)
+        expect(@listener).to receive(:doc_string).with('', '\" \"\"', 1)
         scan(str)
       end
-      
-      it "should lex doc_string content_types" do
+
+      it "lexs doc_string content_types" do
 str = <<EOS
     """gherkin type
     Feature: Doc String Types
     """
 EOS
-        @listener.should_receive(:doc_string).with('gherkin type', 'Feature: Doc String Types', 1)
+        expect(@listener).to receive(:doc_string).with('gherkin type', 'Feature: Doc String Types', 1)
         scan(str)
       end
     end
--- spec/gherkin/shared/encoding_group.rb
+++ spec/gherkin/shared/encoding_group.rb
@@ -3,43 +3,43 @@ module Gherkin
   module Lexer
     shared_examples_for "encoding" do
       describe "with BOM" do
-        it "should work just fine" do
+        it "works just fine" do
           scan_file("with_bom.feature")
-          @listener.to_sexp.should == [
+          expect(@listener.to_sexp).to eq([
             [:feature, "Feature", "Feature Text", "", 1],
             [:scenario, "Scenario", "Reading a Scenario", "", 2],
             [:step, "Given ", "there is a step", 3],
             [:eof]
-          ]
+          ])
         end
 
         describe "and language specification" do
-          it "should work just fine" do
+          it "works just fine" do
             @lexer = Gherkin::Lexer::I18nLexer.new(@listener)
             scan_file("with_bom_and_language_spec.feature")
-            @listener.to_sexp.should == [
-              [:comment, "# language: nl", 1], 
+            expect(@listener.to_sexp).to eq([
+              [:comment, "# language: nl", 1],
               [:feature, "Functionaliteit", "Feature Text", "", 2],
               [:scenario, "Scenario", "Reading a Scenario", "", 3],
               [:step, "Gegeven ", "there is a step", 4],
               [:eof]
-            ]
+            ])
           end
         end
 
         describe "with ISO-8859-1 encoding" do
-          it "should work just fine" do
+          it "works just fine" do
             @lexer = Gherkin::Lexer::I18nLexer.new(@listener)
             scan_file("iso-8859-1.feature")
-            @listener.to_sexp.should == [
-              [:comment, "# language: no", 1], 
+            expect(@listener.to_sexp).to eq([
+              [:comment, "# language: no", 1],
               [:comment, "# encoding: iSo-8859-1", 2],
               [:feature, "Egenskap", "ISO-8859-1", "", 3],
               [:scenario, "Scenario", "ÆØÅ", "", 4],
               [:step, "Når ", "this is encoded as Latin-1", 5],
               [:step, "Så ", "everything should parse", 6],
               [:eof]
-            ]
+            ])
           end
         end
       end
