2d1bcd48b0dfb6a123722871f2dd684681fd8535
John Voloski <johnvoloski@cwi.com.br>
2014-05-21 12:49:48 -0300

    doing update from parser's rspec's syntax

--- spec/gherkin/parser/parser_spec.rb
+++ spec/gherkin/parser/parser_spec.rb
@@ -4,11 +4,11 @@ module Gherkin
   module Parser
     describe Parser do
       unless defined?(JRUBY_VERSION)
-        it "should raise when feature doesn't parse" do
+        it "raises when feature doesn't parse" do
           p = Parser.new(double('formatter').as_null_object)
-          lambda do
+          expect do
             p.parse("Feature: f\nFeature: f", __FILE__, __LINE__-1)
-          end.should raise_error(Regexp.new("Parse error at #{__FILE__}:\\d+"))
+          end.to raise_error(Regexp.new("Parse error at #{__FILE__}:\\d+"))
         end
       end
     end
